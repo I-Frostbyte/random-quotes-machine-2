a919b3232b0840c45e7a47bbc3e4a9f8dc73782a
I-Frostbyte <irefyre@gmail.com>
2022-08-22 17:11:57 +0100

    CORS Implemented but Fetch Failing

--- src/App.js
+++ src/App.js
@@ -1,59 +1,75 @@
-import {useState, useEffect} from 'react';
-import React from 'react';
-import {FaTwitter, FaTumblr} from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import React from "react";
+import { FaTwitter, FaTumblr } from "react-icons/fa";
 
 function App() {
-
-  const [Quotes, setQuotes] = useState('');
-  const [Author, setAuthor] = useState('');
-
+  const [Quotes, setQuotes] = useState("");
+  const [Author, setAuthor] = useState("");
 
   useEffect(() => {
-
     const fetchQuotes = async () => {
-      const request = await fetch("https://zenquotes.io/api/quotes", {mode:'cors'}).then((res) => {
-        return res.json()
+      const request = await fetch("https://zenquotes.io/api/quotes", {
+        type: "GET",
+        headers: "Access-Control-Allow-Origin: *",
       })
+        .then((res) => res.json())
+        .then((results) => console.log(results))
+        .catch((e) => {
+          console.log(e);
+        });
       // setQuotes(request);
-      console.log(request);
-    }
+      // console.log(request);
+    };
 
     fetchQuotes();
-    
+
     const newQuote = () => {
-      const indexQuotes = Math.floor(Math.random() * fetchQuotes.length)
+      const indexQuotes = Math.floor(Math.random() * fetchQuotes.length);
       return fetchQuotes[indexQuotes];
-    }
-  }, [])
-
-
-  
+    };
+  }, []);
 
   const handleClick = () => {
     // setQuotes(newQuote.q);
     // setAuthor(newQuote.a);
-  }
+  };
 
   return (
     <div id="quote-box">
       <div id="content-wrapper">
-        <h1 id="text">{ Quotes }</h1>
-        <p id="author">- { Author }</p>
+        <h1 id="text">{Quotes}</h1>
+        <p id="author">- {Author}</p>
         <div id="below-author">
           <div id="icons">
             <div id="twitter-icon">
-              <a href="twitter.com/intent/tweet" id="tweet-quote"><FaTwitter size={20} style={{color: "#fff"}} className="fas fa-twitter" /></a>
+              <a href="twitter.com/intent/tweet" id="tweet-quote">
+                <FaTwitter
+                  size={20}
+                  style={{ color: "#fff" }}
+                  className="fas fa-twitter"
+                />
+              </a>
             </div>
             <div id="tumblr-icon">
-              <FaTumblr size={20} style={{color: "#fff"}} className="fas fa-tumblr" />
+              <FaTumblr
+                size={20}
+                style={{ color: "#fff" }}
+                className="fas fa-tumblr"
+              />
             </div>
           </div>
           <div id="new-quote-button">
-            <button id="new-quote" onClick={handleClick} style={{color: "#fff"}}>New Quote</button>
+            <button
+              id="new-quote"
+              onClick={handleClick}
+              style={{ color: "#fff" }}
+            >
+              New Quote
+            </button>
           </div>
         </div>
       </div>
-    </div> 
+    </div>
   );
 }
 
